99d445ad29ab978ca7c5480b5c209ce32b29ad33
karlesleith <karlesleith@gmail.com>
2017-12-08 10:04:58 +0000

    Connected to the Marvel Comics API

---
+++ MobileAppsProject2017/MobileAppsProject2017/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MobileAppsProject2017.Models;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MobileAppsProject2017
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public ObservableCollection<Character> MarvelHeroes{ get; set; }
+        public ObservableCollection<ComicBook> MarvelComics { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MarvelHeroes = new ObservableCollection<Character>();
+            MarvelComics = new ObservableCollection<ComicBook>();
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            APIAccess.IsActive = true;
+            APIAccess.Visibility = Visibility.Visible;
+            await MarvelFacade.PopulateMarvelCharactersAsync(MarvelHeroes);
+
+            APIAccess.IsActive = false;
+            APIAccess.Visibility = Visibility.Collapsed;
+        }
+
+        private async void MasterView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+            APIAccess.IsActive = true;
+            APIAccess.Visibility = Visibility.Visible;
+
+            var selectedChar = (Character)e.ClickedItem;
+
+            CharBioName.Text = selectedChar.name;
+            CharBio.Text = selectedChar.description;
+
+            var largeImage = new BitmapImage();
+            Uri uri = new Uri(selectedChar.thumbnail.large);
+
+            largeImage.UriSource = uri;
+
+            CharBioImage.Source = largeImage;
+
+            MarvelComics.Clear();
+
+            await MarvelFacade.PopulateMarvelComicsAsync(selectedChar.id, MarvelComics);
+
+            APIAccess.IsActive = false;
+            APIAccess.Visibility = Visibility.Collapsed;
+
+
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MobileAppsProject2017/MobileAppsProject2017/MarvelFacade.cs
@@ -0,0 +1,156 @@
+﻿using MobileAppsProject2017.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace MobileAppsProject2017
+{
+    public class MarvelFacade
+    {
+        private const string PrivateKey = "f9d25b01acde4a058e4745279142ea856ca10adc";
+        private const string PublicKey = "33948e556bc7277d93fb9f6d95b01efc";
+        private const int MaxCharacters = 1500;
+        private int characterNoId = 1009610;
+
+        public static async Task PopulateMarvelCharactersAsync(ObservableCollection<Character> marvelCharacter)
+        {
+            var characterDataWrapper = await GetCharacterDataWrapperAsync();
+
+            var characters = characterDataWrapper.data.results;
+
+            foreach (var character in characters)
+            {
+                //filter characters miss thumbnail
+                if (character.thumbnail != null && character.description != null)
+                {
+                    marvelCharacter.Add(character);
+                }
+
+                character.thumbnail.small = String.Format("{0}/standard_small.{1}", character.thumbnail.path,character.thumbnail.extension);
+                character.thumbnail.large = String.Format("{0}/portrait_xlarge.{1}", character.thumbnail.path, character.thumbnail.extension);
+            }
+        }
+
+        private static async Task<CharacterDataWrapper> GetCharacterDataWrapperAsync()
+        {
+            // Assemble the URL
+            Random random = new Random();
+            var offset = random.Next(MaxCharacters);
+
+            // Get the MD5 Hash
+            var timeStamp = DateTime.Now.Ticks.ToString();
+            var hash = CreateHash(timeStamp);
+            
+
+
+            string url = String.Format("http://gateway.marvel.com:80/v1/public/characters?limit=10&offset={0}&apikey={1}&ts={2}&hash={3}", offset, PublicKey, timeStamp, hash);
+
+            // Call out to Marvel
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(url);
+            var jsonMessage = await response.Content.ReadAsStringAsync();
+
+            // Response -> string / json -> deserialize
+            var serializer = new DataContractJsonSerializer(typeof(CharacterDataWrapper));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (CharacterDataWrapper)serializer.ReadObject(ms);
+            return result;
+
+            Debug.WriteLine("OUTLOG: ", result);
+        }
+
+        private static async Task<ComicDataWrapper> GetComicDataWrapperAsync(int characterNoId)
+        {
+            // Assemble the URL
+       
+
+            // Get the MD5 Hash
+            var timeStamp = DateTime.Now.Ticks.ToString();
+            var hash = CreateHash(timeStamp);
+
+
+
+            string url = String.Format("https://gateway.marvel.com:443/v1/public/comics?characters={0}&limit=10&apikey={1}&ts={2}&hash={3}", characterNoId, PublicKey, timeStamp, hash);
+
+            // Call out to Marvel
+            HttpClient http = new HttpClient();
+            var response = await http.GetAsync(url);
+            var jsonMessage = await response.Content.ReadAsStringAsync();
+
+            // Response -> string / json -> deserialize
+            var serializer = new DataContractJsonSerializer(typeof(ComicDataWrapper));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonMessage));
+
+            var result = (ComicDataWrapper)serializer.ReadObject(ms);
+            return result;
+
+            Debug.WriteLine("OUTLOG: ", result);
+        }
+
+        public static async Task PopulateMarvelComicsAsync(int characterNoId, ObservableCollection<ComicBook> marvelComic)
+        {
+            try
+            {
+                var comicDataWrapper = await GetComicDataWrapperAsync(characterNoId);
+
+                var comics = comicDataWrapper.data.results;
+
+                foreach (var comic in marvelComic)
+                {
+
+
+                    if (comic.thumbnail != null
+                        && comic.thumbnail.path != "")
+                    {
+
+                        comic.thumbnail.small = String.Format("{0}/portrait_medium.{1}",
+                            comic.thumbnail.path,
+                            comic.thumbnail.extension);
+
+                        comic.thumbnail.large = String.Format("{0}/portrait_xlarge.{1}",
+                            comic.thumbnail.path,
+                            comic.thumbnail.extension);
+
+                        marvelComic.Add(comic);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
+
+        private static string CreateHash(string timeStamp)
+        {
+
+            var toBeHashed = timeStamp + PrivateKey + PublicKey;
+            var hashedMessage = ComputeMD5(toBeHashed);
+            return hashedMessage;
+        }
+
+        // From:
+        // http://stackoverflow.com/questions/8299142/how-to-generate-md5-hash-code-for-my-winrt-app-using-c
+        private static string ComputeMD5(string str)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+            return res;
+        }
+
+    }
+}
